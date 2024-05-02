112a793b08ac2228d94e6a1c6da304374c0d8276
hungvt0806 <67869124+hungvt0806@users.noreply.github.com>
2024-05-02 16:37:55 +0900

    filter sort phan trang

--- server/controllers/quizController.js
+++ server/controllers/quizController.js
@@ -1,23 +1,50 @@
 const Quiz = require('../models/QuizModel');
 const Question = require('../models/QuestionModel');
 
-//get all  quizes
-exports.getAllQuizzes = async (req,res,next)=>{
-    try{
-        const quizzes = await Quiz.find({}).populate('author');
+//get all  quizes+ Pagination + Sort + Filter
+exports.getAllQuizzes = async (req, res, next) => {
+    try {
+        const page = parseInt(req.query.page) || 1; // Số trang mặc định là 1 nếu không có truy vấn
+        const limit = parseInt(req.query.limit) || 10; // Số lượng phần tử trên mỗi trang, mặc định là 10
+        const sortBy = req.query.sortBy || '_id'; // Trường để sắp xếp, mặc định là 'createdAt'
+        const sortOrder = req.query.sortOrder || 'asc'; // Hướng sắp xếp, mặc định là 'desc'
+
+        const startIndex = (page - 1) * limit; // Vị trí bắt đầu của trang hiện tại
+
+        let filter = {};
+        // Xử lý các tham số filter từ query string của URL (ví dụ: /api/quizzes?type=languae)
+        if (req.query.type) {
+            filter.type = req.query.type;
+        }
+        // Các điều kiện filter khác có thể được xử lý tương tự
+
+        const totalQuizzes = await Quiz.countDocuments(filter); // Tổng số lượng quizzes
+
+        const totalPages = Math.ceil(totalQuizzes / limit); // Tổng số trang
+
+        const quizzes = await Quiz.find(filter)
+            .populate('author')
+            .sort({ [sortBy]: sortOrder }) // Sắp xếp theo trường và hướng đã chỉ định
+            .skip(startIndex) // Bỏ qua các phần tử từ vị trí bắt đầu
+            .limit(limit); // Giới hạn số lượng phần tử trả về
+
         res.status(200).json({
-            status : 'success',
+            status: 'success',
             results: quizzes.length,
-            data:{quizzes}
-        })
+            currentPage: page,
+            totalPages: totalPages,
+            data: { quizzes }
+        });
         
-    }catch (error){
+    } catch (error) {
         return res.status(500).json({
             status: 'ERR',
             message: error.message || 'Internal server error'
-    });
+        });
     }
 }
+
+
 
 //create one  quiz
 exports.createOneQuiz = async (req,res,next)=>{
