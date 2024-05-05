69f2b011f1e05b4afec69b7ebc95321072d5bb95
hungvt0806 <67869124+hungvt0806@users.noreply.github.com>
2024-05-05 23:32:25 +0900

    ranking

--- client/src/components/Ranking.jsx
+++ client/src/components/Ranking.jsx
@@ -1,29 +1,18 @@
-import React from 'react'
+import React from "react";
+import AccountRanking from "./AccountRanking";
+import jsonData from "./data.json";
 
 const Ranking = () => {
   return (
-    <div className =" flex w-full h-screen flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-  <div className="p-6 flex items-center flex-col">
-    <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-      RANKING
-    </h5>
-    <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-      The place is close to Barceloneta Beach and bus stop just 2 min by walk
-      and near to "Naviglio" where you can enjoy the main night life in
-      Barcelona.
-    </p>
-  </div>
-  <div className="flex items-center justify-center">
-    <button
-      className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button"
-      data-ripple-light="true"
-    >
-      Read More
-    </button>
-  </div>
-</div>
-  )
-}
+    <div className="container flex-col mx-auto  bg-gray-100 rounded-xl bg-clip-border w-full h-[480px] mr-1 mt-0 shadow-md">
+      <h4 className="pt-3 pb-4  px-auto text-center font-bold  ">RANKING</h4>
+    
+      <hr className="w-full border-0 border-b-2 bg-white" />
+      <div className="overflow-y-auto overflow-x-hidden h-5/6">
+        <AccountRanking items={jsonData} />
+      </div>
+    </div>
+  );
+};
 
-export default Ranking
+export default Ranking;
